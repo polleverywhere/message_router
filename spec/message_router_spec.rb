d6e4f68c6c8b16f3f028dedbfa24f715fa9a6865
Paul Cortens <paul@thoughtless.ca>
2012-04-26 13:36:14 -0700

    add pending specs

--- spec/message_router_spec.rb
+++ spec/message_router_spec.rb
@@ -7,6 +7,17 @@ describe MessageRouter::Router do
       r = MessageRouter::Router.build {}
       r.should be_kind_of MessageRouter::Router
     end
+
+    describe 'defining matchers' do
+      it 'accepts a boolean'
+      it 'accepts a proc which is passed the message'
+      it 'accepts a regex to match against the message body'
+      it 'accepts a string to match against the message body'
+      describe 'matching a hash' do
+        it 'accepts a string to match against the hash key'
+        it 'accepts a regex to match against the hash key'
+      end
+    end
   end
 
   describe "#call" do
@@ -30,6 +41,16 @@ describe MessageRouter::Router do
         subject.call({})
         $did_it_run.should be_true
       end
+    end
+
+    it 'supports nested routers' do
+      pending
+      main_router = MessageRouter::Router.build do
+        sub_router = MessageRouter::Router.build do
+          match
+        end
+      end
+
     end
   end
 end
